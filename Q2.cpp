ce41f36aac3c7075a11122521d7ffbaf360513cd
suhelsaifi0 <suhelsaifi0001@gmail.com>
2026-01-20 11:21:22 +0530

    first commit

---
+++ Q2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+ int main(){
+    
+    //  Print 5 Star in a Row
+    int n;
+    cout<<"ENTER NUMBER : ";
+    cin>>n;
+
+    for(int i=0; i<n; i++){
+        cout<<"* ";
+    }
+    
+    return 0;
+}
